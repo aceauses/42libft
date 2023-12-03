a2657c87d46399f601df967eb811c771ab209382
aceauses <aceauses@student.42heilbronn>
2023-12-03 16:52:50 +0100

    fix strdup not segfault in minishell

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aceauses <aceauses@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: aceauses <aceauses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 15:26:43 by aceauses          #+#    #+#             */
-/*   Updated: 2023/04/12 13:51:12 by aceauses         ###   ########.fr       */
+/*   Updated: 2023/12/03 16:52:41 by aceauses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,10 @@ char	*ft_strdup(const char *str)
 	size_t	l;
 	size_t	i;
 
+	if (str == NULL)
+		return (NULL);
 	l = ft_strlen(str);
 	memory = (char *)malloc(l + 1);
-	if (str == NULL)
-	{
-		return (memory);
-	}
 	if (memory == NULL)
 		return (0);
 	i = 0;
